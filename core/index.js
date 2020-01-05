6ca3e732be3b4d7c6b5d589da1f6e8e2f3617c94
Krishna Sagar R <ragas.sagar.474@gmail.com>
2020-01-05 21:09:36 +0000

    add image to README.md

--- core/index.js
+++ core/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from "redux";
+import { createCollectionReducer, selectCollection } from "./datastructures";
 
-export const createSpace = () => {
+export const createSpace = ({ namespace }) => {
   let currentReducers = {
     init: (state = {}) => state
   };
@@ -9,12 +10,58 @@ export const createSpace = () => {
   return {
     putSlice: ({ sliceName }) => {
       if (typeof currentReducers[sliceName] !== "function") {
-        currentReducers[sliceName] = (state = {}) => {
-          return state;
-        };
+        currentReducers[`${namespace}_${sliceName}`] = createCollectionReducer({
+          initialCollection: [],
+          updateWhen: action =>
+            action.type === `${namespace}_${sliceName}_UPDATED`,
+          addWhen: action => action.type === `${namespace}_${sliceName}_ADDED`,
+          removeWhen: action =>
+            action.type === `${namespace}_${sliceName}_REMOVED`,
+          enhancers: []
+        });
       }
       store.replaceReducer(combineReducers(currentReducers));
     },
-    store
+    store,
+    addToCollection: ({ sliceName, entity }) => {
+      store.dispatch({
+        type: `${namespace}_${sliceName}_ADDED`,
+        data: entity
+      });
+    },
+    updateInCollection: ({ sliceName, entity }) => {
+      store.dispatch({
+        type: `${namespace}_${sliceName}_UPDATED`,
+        data: entity
+      });
+    },
+    removeFromCollection: ({ sliceName, entity }) => {
+      store.dispatch({
+        type: `${namespace}_${sliceName}_REMOVED`,
+        data: entity
+      });
+    },
+    collectionLoading: ({ sliceName, entity }) => {
+      store.dispatch({
+        type: `${namespace}_${sliceName}_LOADING`,
+        data: entity
+      });
+    },
+    collectionLoaded: ({ sliceName, entity }) => {
+      store.dispatch({
+        type: `${namespace}_${sliceName}_LOADED`,
+        data: entity
+      });
+    },
+    collectionAborted: ({ sliceName, entity }) => {
+      store.dispatch({
+        type: `${namespace}_${sliceName}_ABORTED`,
+        data: entity
+      });
+    }
   };
+};
+
+export const selectSliceFromState = (state, sliceName) => {
+  return selectCollection(state[sliceName]);
 };
